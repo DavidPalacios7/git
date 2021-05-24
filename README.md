83a3f014ab9692813d92ceab480ccab17a8c1970
David Palacios <davidpalaciossilva7@gmail.com>
2021-05-23 21:10:39 -0500

    [Hotfix] Add the chenges of hotfix branch into code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git
 Project to git skills
+
+Add the hotfix changes to the project from hotfix branch
---
+++ src/main/java/com/colombia/git/GitApplication.java
@@ -0,0 +1,17 @@
+package com.colombia.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+	public static void main(String[] args) {
+
+		/*
+			Add hotfix changes in the code
+		 */
+		SpringApplication.run(GitApplication.class, args);
+	}
+
+}
